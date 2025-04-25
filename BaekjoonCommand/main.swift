d3c369eb98ba503abde0eece1960d8b325afbaa3
Park Gwonyong <rnjsdydqkr@naver.com>
2025-04-25 10:45:08 +0900

    BFS, DFS 주석처리

--- BaekjoonCommand/main.swift
+++ BaekjoonCommand/main.swift
@@ -11,38 +11,38 @@ import Foundation
 // BFS
 // *********************
 
-let graph = [
-  [],
-  [2, 3, 8],
-  [1, 7],
-  [1, 4, 5],
-  [3, 5],
-  [3, 4],
-  [7],
-  [2, 6, 8],
-  [1, 7]
-]
-var visited = Array(repeating: false, count: 9)
-
-func bfs(graph: [[Int]], start: Int, visited: inout [Bool]) {
-  var queue: [Int] = []
-  queue.append(start)
-  visited[start] = true
-  
-  while !queue.isEmpty {
-    let v = queue.removeFirst()
-    print(v, terminator: " ")
-
-    for i in graph[v] {
-      if !visited[i] {
-        queue.append(i)
-        visited[i] = true
-      }
-    }
-  }
-}
-
-bfs(graph: graph, start: 1, visited: &visited)
+//let graph = [
+//  [],
+//  [2, 3, 8],
+//  [1, 7],
+//  [1, 4, 5],
+//  [3, 5],
+//  [3, 4],
+//  [7],
+//  [2, 6, 8],
+//  [1, 7]
+//]
+//var visited = Array(repeating: false, count: 9)
+//
+//func bfs(graph: [[Int]], start: Int, visited: inout [Bool]) {
+//  var queue: [Int] = []
+//  queue.append(start)
+//  visited[start] = true
+//  
+//  while !queue.isEmpty {
+//    let v = queue.removeFirst()
+//    print(v, terminator: " ")
+//
+//    for i in graph[v] {
+//      if !visited[i] {
+//        queue.append(i)
+//        visited[i] = true
+//      }
+//    }
+//  }
+//}
+//
+//bfs(graph: graph, start: 1, visited: &visited)
 
 // *********************
 // DFS
@@ -63,7 +63,7 @@ bfs(graph: graph, start: 1, visited: &visited)
 //
 //func dfs(graph: [[Int]], v: Int, visited: inout [Bool]) {
 //  visited[v] = true
-//  print("\(v) ", terminator: "")
+//  print(v, terminator: " ")
 //  for i in graph[v] {
 //    if !visited[i] {
 //      dfs(graph: graph, v: i, visited: &visited)
