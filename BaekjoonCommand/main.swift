9b95fb1b971c82acbc13643ece34a0a8ea3296fa
Park Gwonyong <rnjsdydqkr@naver.com>
2025-05-09 10:29:31 +0900

    퀵정렬

--- BaekjoonCommand/main.swift
+++ BaekjoonCommand/main.swift
@@ -8,6 +8,52 @@
 import Foundation
 
 // *********************
+// 퀵 정렬
+// *********************
+
+/*
+5, 7, 9, 0, 3, 1, 6, 2, 4, 8
+
+pivot 0
+left  1
+right 9
+start 0
+end   9
+*/
+
+var array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+var pivot = 0
+var left = 0
+var right = 0
+
+func quick_sort(_ array: inout [Int], _ start: Int, _ end: Int) {
+  if start >= end {
+    return
+  }
+  pivot = start
+  left = start + 1
+  right = end
+  while(left <= right) {
+    while(left <= end && array[left] <= array[pivot]) {
+      left += 1
+    }
+    while(right > start && array[right] >= array[pivot]) {
+      right -= 1
+    }
+    if left > right {
+      (array[right], array[pivot]) = (array[pivot], array[right])
+    } else {
+      (array[left], array[right]) = (array[right], array[left])
+    }
+  }
+  quick_sort(&array, start, right - 1)
+  quick_sort(&array, right + 1, end)
+}
+
+quick_sort(&array, 0, array.count - 1)
+print(array)
+
+// *********************
 // 삽입 정렬
 // *********************
 
@@ -24,19 +70,19 @@ i 1 2 3 4
 j 1 2 b 3 2 1 4 3 2
 */
 
-var array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
-
-for i in 1 ..< array.count {
-  for j in stride(from: i, to: 0, by: -1) {
-    if array[j] < array[j - 1] {
-      (array[j], array[j - 1]) = (array[j - 1], array[j])
-    } else {
-      break
-    }
-  }
-}
-
-print(array)
+//var array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+//
+//for i in 1 ..< array.count {
+//  for j in stride(from: i, to: 0, by: -1) {
+//    if array[j] < array[j - 1] {
+//      (array[j], array[j - 1]) = (array[j - 1], array[j])
+//    } else {
+//      break
+//    }
+//  }
+//}
+//
+//print(array)
 
 // *********************
 // 선택 정렬
